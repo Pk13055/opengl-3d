e638e05262066dc3e45444540907c3f07125d55d
pk13055 <pkrockstar7@gmail.com>
2018-03-05 18:11:48 +0530

    Sea complete with motion

--- src/sea.cpp
+++ src/sea.cpp
@@ -3,10 +3,11 @@
 
 #define SEA_WIDTH 400
 #define SEA_LENGTH 400
-#define SEA_TURBULENCE 0.2f
+#define SEA_TURBULENCE 0.002f
 
 Sea::Sea(float x, float y, color_t color) {
-    this->position = glm::vec3(x, y, 0);
+    this->position = glm::vec3(x, y + 0.25f, 0);
+    this->yspeed = SEA_TURBULENCE;
     float x_coord = SEA_WIDTH / 2,
     z_coord = SEA_LENGTH / 2;
     static const GLfloat vertex_buffer_data[] = {
@@ -25,8 +26,7 @@ Sea::Sea(float x, float y, color_t color) {
 void Sea::draw(glm::mat4 VP) {
     Matrices.model = glm::mat4(1.0f);
     glm::mat4 translate = glm::translate (this->position);    // glTranslatef
-    glm::mat4 rotate    = glm::rotate((float) (this->rotation * M_PI / 180.0f), glm::vec3(1, 0, 0));
-    Matrices.model *= (translate * rotate);
+    Matrices.model *= (translate);
     glm::mat4 MVP = VP * Matrices.model;
     glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
     draw3DObject(this->object);
@@ -38,7 +38,8 @@ void Sea::set_position(float x, float z) {
 
 void Sea::tick() {
     this->position.y += yspeed;
-    this->yspeed += ((this->position.y <= 0.0)? 1 : -1 ) * SEA_TURBULENCE;
-    this->position.y =  ((this->position.y > 2.5f)? 1 : -1 ) * 2.5f;
+    this->yspeed += ((this->position.y <= 0.0f)? 1 : -1 ) * SEA_TURBULENCE;
+    if(this->position.y > 0.35f) this->position.y = 0.35f;
+    else if(this->position.y < -0.35f) this->position.y = -0.35f;
 }
 
