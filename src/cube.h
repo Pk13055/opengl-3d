13c53014b3b42ae406b15abfd64781d1a4fbc051
pk13055 <pkrockstar7@gmail.com>
2018-03-05 18:09:11 +0530

    Cube primitive in place
    - Basic cube shape drawing
    - Proper rendering with rotation

--- src/cube.h
+++ src/cube.h
@@ -3,20 +3,20 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+
 class Cube {
 public:
-	Cube() {}
-	Cube(float x, float y, float z, float width, float height, float depth, color_t color);
-	glm::vec3 position, speed, size;
-	color_t color;
-	float rotation = 0;
-	void draw(glm::mat4 VP);
-	void set_position(float x, float y, float z);
-	void set_dimensions(float width, float height, float depth);
-	void set_speed(float x_speed, float y_speed, float z_speed);
-	bounding_box_t bounding_box();
+    Cube() {}
+    Cube(float x, float y,float z,float width , float height , float depth,  color_t color);
+    glm::vec3 position, speed, size;
+    float rotation = 0;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y, float z);
+    void set_dimensions(float width, float height, float depth);
+    void set_speed(float x_speed, float y_speed, float z_speed);
+    bounding_box_t bounding_box();
 private:
-	VAO *object;
+    VAO *object;
 };
 
 #endif // CUBE_H
